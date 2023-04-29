6e878890b4c89d2368de6b354fa6422fb5a848d0
Eduardo García <eduardogarcia101125@gmail.com>
2023-04-29 11:40:03 -0600

    Agregar archivos de proyecto.

---
+++ GuiaPractica22-05-23/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuiaPractica22_05_23.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
